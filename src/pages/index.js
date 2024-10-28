f5a78caff322c75b58a7f137548f91313a2d1a89
Sabareesh001 <hatrickda@gmail.com>
2024-10-28 18:06:06 +0530

    hot fixed layout import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import Profile from "./profile/profile";
-import TeamPage from "./team/Team";
-import FeedbackPage from "./feedback/feedback";
-import Settings from "./setting/settings";
+import Profile from "./profile/profile.jsx";
+import TeamPage from "./team/team.jsx";
+import FeedbackPage from "./feedback/feedback.jsx";
+import Settings from "./setting/settings.jsx";
 
 export { Profile, TeamPage, FeedbackPage, Settings };
